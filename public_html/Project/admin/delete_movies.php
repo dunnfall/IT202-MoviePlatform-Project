72013b66f3cd8fe297f942692cf88d9847bdbac3
dunnfall <danthrman203@gmail.com>
2024-04-19 15:11:45 -0400

    View Singular Movie Data Page

--- public_html/Project/admin/delete_movies.php
+++ public_html/Project/admin/delete_movies.php
@@ -1,18 +1,18 @@
 <?php
 
-require(__DIR__ . "/../../../partials/nav.php");
+session_start();
+require(__DIR__ . "/../../../lib/functions.php");
 
+// require(__DIR__ . "/../../../lib/nav.php");
 if (!has_role("Admin")) {
     flash("You don't have permission to view this page", "warning");
     die(header("Location: " . get_url("admin/list_movies.php")));
 }
 
-session_start();
-require(__DIR__ . "/../../../lib/functions.php");
 
 $id = se($_GET, "id", -1, false);
 if ($id < 1) {
-    flash("Invalid id passed to delete", "danger");
+    flash("Invalid Id Passed", "danger");
     die(header("Location: " . get_url("admin/list_movies.php")));
 }
 
@@ -21,10 +21,10 @@ $query = "DELETE FROM `Movies` WHERE id = :id";
 try {
     $stmt = $db->prepare($query);
     $stmt->execute([":id" => $id]);
-    flash("Deleted record with id $id", "success");
+    flash("Deleted Record With ID $id", "success");
 } catch (Exception $e) {
-    error_log("Error deleting stock $id" . var_export($e, true));
-    flash("Error deleting record", "danger");
+    error_log("Error Deleting Movie $id" . var_export($e, true));
+    flash("Error Deleting Movie", "danger");
 }
 die(header("Location: " . get_url("admin/list_movies.php")));
 
---
+++ public_html/Project/admin/view_movies.php
@@ -0,0 +1,73 @@
+<?php
+//note we need to go up 1 more directory
+require(__DIR__ . "/../../../partials/nav.php");
+
+if (!has_role("Admin")) {
+    flash("You don't have permission to view this page", "warning");
+    die(header("Location: $BASE_PATH" . "/home.php"));
+}
+?>
+
+<?php
+$id = se($_GET, "id", -1, false);
+
+
+$broker = [];
+if ($id > -1) {
+    //fetch
+    $db = getDB();
+    $query = "SELECT id, title, year, imdb_id, source, created, modified FROM `Movies` WHERE id = :id";
+    try {
+        $stmt = $db->prepare($query);
+        $stmt->execute([":id" => $id]);
+        $r = $stmt->fetch();
+        if ($r) {
+            $broker = $r;
+        } else {
+            flash("Invalid ID Passed, Use a Valid ID", "danger");
+            die(header("Location:" . get_url("admin/list_movies.php")));
+        }
+    } catch (PDOException $e) {
+        error_log("Error fetching record: " . var_export($e, true));
+        flash("Error fetching record", "danger");
+    }
+} 
+foreach ($broker as $key => $value) {
+    if (is_null($value)) {
+        $broker[$key] = "N/A";
+    }
+}
+//TODO handle manual create stock
+?>
+<div class="container-fluid">
+    <h3>Movie: <?php se($broker, "title", "Unknown"); ?></h3>
+    <div>
+        <a href="<?php echo get_url("admin/list_movies.php"); ?>" class="btn btn-secondary">Back</a>
+    </div>
+    <!-- https://i.kym-cdn.com/entries/icons/original/000/029/959/Screen_Shot_2019-06-05_at_1.26.32_PM.jpg -->
+    <div class="card mx-auto">
+        <!-- <img src="" class="card-img-top" alt="..."> -->
+        <div class="card-body">
+            <h5 class="card-title"><?php se($broker, "title", "Unkown"); ?> </h5>
+            <div class="card-text">
+                <ul class="list-group">
+                    <li class="list-group-item">Movie: <?php se($broker, "title", "Unknown"); ?></li>
+                    <li class="list-group-item">Year: <?php se($broker, "year", "Unknown"); ?></li>
+                    <li class="list-group-item">IMDB ID: <?php se($broker, "imdb_id", "Unknown"); ?></li>
+                    <li class="list-group-item">Source: <?php se($broker, "source", "Unknown"); ?></li>
+                    <li class="list-group-item">ID: <?php se($broker, "id", "Unknown"); ?></li>
+                    <li class="list-group-item">Created: <?php se($broker, "created", "Unknown"); ?></li>
+                    <li class="list-group-item">Modified: <?php se($broker, "modified", "Unknown"); ?></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<?php
+//note we need to go up 1 more directory
+require_once(__DIR__ . "/../../../partials/flash.php");
+?>
